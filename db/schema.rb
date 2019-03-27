8a979007c5f1bbbe4951633380d7ebfe7b8b291a
github username <github email address>
2019-03-27 14:06:10 -0700

    Still not working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_162911) do
+ActiveRecord::Schema.define(version: 2019_03_26_171722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 2019_03_26_162911) do
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.string "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
